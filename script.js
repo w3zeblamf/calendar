64fe5f48541bd021127a165b34a11b6349f3c041
w3zeblamf <zeblamfw3@gmail.com>
2022-07-19 20:25:04 -0400

    2 files modified

--- script.js
+++ script.js
@@ -5,17 +5,25 @@ const date = new Date();
 //console.log(date);
 //console.log(month);
 
-const month = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'DEcember',
+const months = [
+  'January', // [0]
+  'February', // [1]
+  'March', // [2]
+  'April', // [3]
+  'May', // [4]
+  'June', // [5]
+  'July', // [6]
+  'August', // [7]
+  'September', // [8]
+  'October', // [9]
+  'November', // [10]
+  'December', // [11]
 ];
+
+document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+
+document.querySelector('.date p').innerHTML = date.toDateString();
+
+let days = '';
+
+for (let i = 1; i <= 31; i++) {}
