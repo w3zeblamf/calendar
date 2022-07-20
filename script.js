4c1ddcbce3f16e46f3a788f5f9073781ff38eb05
w3zeblamf <zeblamfw3@gmail.com>
2022-07-19 20:01:36 -0400

    1 file modified

---
+++ script.js
@@ -0,0 +1,21 @@
+const date = new Date();
+
+//const month = date.getMonth();
+
+//console.log(date);
+//console.log(month);
+
+const month = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'DEcember',
+];
